394032691fefe1b085bde0752681ea4c7aab5bc8
Shanmukh <kondan1120@gmail.com>
2025-04-13 13:49:59 +0530

    changes made to api appProducts.js

--- src/api/allProducts.js
+++ src/api/allProducts.js
@@ -1,24 +1,3 @@
-{// import axios from "axios";
-// const baseURL = 'https://api.escuelajs.co/api/v1';
-// // export const getAllProducts = async ()=>{
-//     const url = `${baseURL}/products`;
-// //     try{
-// //         const response = await fetch(url)
-// //         if(!response.ok){
-// //             alert("HTTP Error");            
-// //         }
-// //         const data = await response.json();
-// //         return data;
-// //     }
-// //     catch(error){
-// //         alert("ERROR WHILE FETCHING DATA....")
-// //     }
-// // }
-// const getAllProducts = axios.get(url)
-// const products = getAllProducts.then(res=>res)
-// console.log(products)
-// console.log(getAllProducts)
-}
 import axios from "axios";
 
 const baseURL = 'https://fakestoreapi.in/api';
